5dd004470a96ece0aa8eddb1b9ac8e10ac5ea133
pmezquita <ing.pedromezquita@gmail.com>
2023-07-07 15:54:33 -0600

    [ADD] Limpiar selector de paises cuando no se ha seleccionado region

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {CountriesService} from "../../services/countries.service";
 import {Region, SmallCountry} from "../../interfaces/country.interface";
-import {switchMap} from "rxjs";
+import {switchMap, tap} from "rxjs";
 
 @Component({
   selector: 'app-selector-page',
@@ -36,7 +36,8 @@ export class SelectorPageComponent implements OnInit {
   private onRegionChanged = (): void => {
     this.myForm.get('region')?.valueChanges
       .pipe(
-        switchMap(region => this.countriesService.getCountriesByRegion(region)),
+        tap(() => this.myForm.get('country')?.setValue('')),
+        switchMap((region) => this.countriesService.getCountriesByRegion(region)),
       )
       .subscribe(countries => {
         this.countriesByRegion = countries;
